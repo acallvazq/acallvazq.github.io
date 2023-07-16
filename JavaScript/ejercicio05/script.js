823d0988888174f6eceec4423fecde144a1287ff
Alba Vazquez <runo-mola12@hotmail.es>
2023-07-17 00:46:59 +0200

    Ejercicio 5 sin terminar

---
+++ JavaScript/ejercicio05/script.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+    var contenido = document.getElementById("contenido");
+    var hora = "00";
+    var minutos = "00";
+    var segundos = "00";
+
+    contenido.innerHTML = hora + ":" + minutos + ":" + segundos;
+};
+
+function actualizaSegundos(pSegundos) {
+    var contenido = document.getElementById("contenido");
+    var hora = "0";
+    var minutos = "0";
+    var segundos = "0";
+
+    if(!isNaN(pSegundos) && pSegundos !== ""){
+        segundos = parseInt(pSegundos);
+        if(segundos > 59) {
+            minutos = Math.floor(segundos / 60);
+            segundos = segundos % 60; 
+        }if(minutos > 59){
+            hora = Math.floor(minutos / 60);
+            minutos = minutos % 60; 
+
+        }if(hora >= 24){
+            hora = 24;
+            minutos = 0;
+            segundos = 0;
+        }
+    }
+
+    hora = (hora < 10) ? "0" + hora : hora;
+    minutos = (minutos < 10) ? "0" + minutos : minutos;
+    segundos = (segundos < 10) ? "0" + segundos : segundos;
+
+    contenido.innerHTML = hora + ":" + minutos + ":" + segundos;
+}
